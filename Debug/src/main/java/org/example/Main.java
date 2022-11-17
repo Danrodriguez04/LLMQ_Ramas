5584045c77fc8a98175ce4f6736d297f087a02de
Danrodriguez04 <danielsantiagorodriguez72@gmail.com>
2022-11-17 18:40:37 +0100

    Practica git

---
+++ Debug/src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+
+
+        int [] lista = {2,3,4,6,2,4,2,4};
+
+        for (int i = 0; i < lista.length; i++);
+        System.out.println( lista[i]);
+    }
+}
